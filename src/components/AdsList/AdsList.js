4c75dd4870f0ff8d23e97d31452ae59a19fc66d8
Stanislav Sergienya <skilful522@gmail.com>
2019-11-01 15:34:08 +0300

    minor fix

--- src/components/AdsList/AdsList.js
+++ src/components/AdsList/AdsList.js
@@ -4,17 +4,47 @@ import { Warning } from "./Warning/Warning";
 import { Item } from "./Item/Item";
 import style from "./AdsList.module.css";
 import uuidv4 from "uuid/v4";
-import { dataHelper } from "../../utils/data.helper";
+import fetchJsonp from "fetch-jsonp";
+import { dataHelper, dataProcessing } from "../../utils/data.helper";
+import { useState, useEffect } from "react";
+
+const useFetch = (url, props) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const fetchData = () => {
+    fetchJsonp(url)
+      .then(response => response.json())
+      .then(data => {
+        if (data.response["application_response_text"] === "unknown location") {
+          setError(true);
+          setLoading(false);
+        } else {
+          setError(false);
+          setData(data);
+          dataProcessing(data, props);
+          setLoading(false);
+        }
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
 
 const AdsList = props => {
   const url = dataHelper.constructQueryParams(props);
-  const { loading, error } = dataHelper.useFetch(url, props);
+  const { loading, error } = useFetch(url, props);
 
   if (loading) {
     return <Loader />;
-  } else if (error) {
+  }
+  if (error) {
     return <Warning />;
-  } else {
+  }
+  {
     return (
       <div id={style["adsList"]}>
         {props.flats.map(item => (
--- src/utils/data.helper.js
+++ src/utils/data.helper.js
@@ -1,21 +1,17 @@
-import React from "react";
-import fetchJsonp from "fetch-jsonp";
-import { useState, useEffect } from "react";
-
 function dataProcessing(data, props) {
   const receivedFlats = data.response.listings;
   const flatImages = receivedFlats.map(flat => flat["img_url"]);
   const flatTitles = receivedFlats.map(flat => flat["title"]);
   const flatProperties = receivedFlats.map(flat => {
     return [
-      <b>{data.request.location}</b>,
+      data.request.location,
       " • ",
       flat["bedroom_number"],
       " Bedroom • ",
       flat["bathroom_number"],
       " Bath •",
       " ",
-      <b>{flat["property_type"]}</b>,
+      flat["property_type"],
       " • ",
       flat["keywords"]
     ];
@@ -35,33 +31,6 @@ function dataProcessing(data, props) {
 }
 
 const dataHelper = {
-  useFetch: (url, props) => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-    const fetchData = () => {
-      fetchJsonp(url)
-        .then(response => response.json())
-        .then(data => {
-          if (
-            data.response["application_response_text"] === "unknown location"
-          ) {
-            setError(true);
-            setLoading(false);
-          } else {
-            setError(false);
-            setData(data);
-            dataProcessing(data, props);
-            setLoading(false);
-          }
-        });
-    };
-    useEffect(() => {
-      fetchData();
-    }, [url]);
-
-    return { data, loading, error };
-  },
   constructQueryParams: ({ searchInput }) => {
     const url = "https://api.nestoria.co.uk/api?";
     let params = new URLSearchParams();
@@ -77,4 +46,4 @@ const dataHelper = {
   }
 };
 
-export { dataHelper };
+export { dataHelper, dataProcessing };
