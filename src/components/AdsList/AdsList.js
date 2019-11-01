373b84938b58a5535ea2cc3a47b3dceaebf90690
Stanislav Sergienya <skilful522@gmail.com>
2019-11-01 12:40:12 +0300

    minor fix

--- src/components/AdsList/AdsList.js
+++ src/components/AdsList/AdsList.js
@@ -2,51 +2,34 @@ import React from "react";
 import { Loader } from "./Loader/Loader";
 import { Warning } from "./Warning/Warning";
 import { Item } from "./Item/Item";
-import style from "./AdsList.module.css"
+import style from "./AdsList.module.css";
 import uuidv4 from "uuid/v4";
-import {dataHelper} from "../../utils/data.helper";
+import { dataHelper } from "../../utils/data.helper";
 
 const AdsList = props => {
-  const url = constructQueryParams(props);
-  const {loading, error } = dataHelper.useFetch(url, props);
-  return (
-    <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <div id={style["adsList"]}>
-          {error ? (
-            <Warning />
-          ) : (
-            props.flats.map(item => (
-              <Item
-                image={item.flatImage}
-                title={item.flatTitle}
-                property={item.flatProperty}
-                summary={item.flatSummary}
-                price={item.flatPrice}
-                key={uuidv4()}
-              />
-            ))
-          )}
-        </div>
-      )}
-    </>
-  );
+  const url = dataHelper.constructQueryParams(props);
+  const { loading, error } = dataHelper.useFetch(url, props);
+
+  if (loading) {
+    return <Loader />;
+  } else if (error) {
+    return <Warning />;
+  } else {
+    return (
+      <div id={style["adsList"]}>
+        {props.flats.map(item => (
+          <Item
+            image={item.flatImage}
+            title={item.flatTitle}
+            property={item.flatProperty}
+            summary={item.flatSummary}
+            price={item.flatPrice}
+            key={uuidv4()}
+          />
+        ))}
+      </div>
+    );
+  }
 };
-
-function constructQueryParams({ searchInput }) {
-  const url = "https://api.nestoria.co.uk/api?";
-  let params = new URLSearchParams();
-
-  params.append("encoding", "json");
-  params.append("pretty", "1");
-  params.append("action", "search_listings");
-  params.append("country", "uk");
-  params.append("listing_type", "rent");
-  params.append("page", `1`);
-  params.append("place_name", `${searchInput}`);
-  return url + params;
-}
 
 export { AdsList };
--- src/utils/data.helper.js
+++ src/utils/data.helper.js
@@ -1,65 +1,80 @@
-import React from 'react'
+import React from "react";
 import fetchJsonp from "fetch-jsonp";
 import { useState, useEffect } from "react";
 
-function dataProcessing (data, props) {
-    const receivedFlats = data.response.listings;
-    const flatImages = receivedFlats.map(flat => flat["img_url"]);
-    const flatTitles = receivedFlats.map(flat => flat["title"]);
-    const flatProperties = receivedFlats.map(flat => {
-        return [
-            <b>{data.request.location}</b>,
-            " • ",
-            flat["bedroom_number"],
-            " Bedroom • ",
-            flat["bathroom_number"],
-            " Bath •",
-            " ",
-            <b>{flat["property_type"]}</b>,
-            " • ",
-            flat["keywords"]
-        ];
-    });
-    const flatPrice = receivedFlats.map(flat => `${flat["price_formatted"]} ${flat["price_type"]}`);
-    const flatSummary = receivedFlats.map(flat => flat["summary"]);
+function dataProcessing(data, props) {
+  const receivedFlats = data.response.listings;
+  const flatImages = receivedFlats.map(flat => flat["img_url"]);
+  const flatTitles = receivedFlats.map(flat => flat["title"]);
+  const flatProperties = receivedFlats.map(flat => {
+    return [
+      <b>{data.request.location}</b>,
+      " • ",
+      flat["bedroom_number"],
+      " Bedroom • ",
+      flat["bathroom_number"],
+      " Bath •",
+      " ",
+      <b>{flat["property_type"]}</b>,
+      " • ",
+      flat["keywords"]
+    ];
+  });
+  const flatPrice = receivedFlats.map(
+    flat => `${flat["price_formatted"]} ${flat["price_type"]}`
+  );
+  const flatSummary = receivedFlats.map(flat => flat["summary"]);
 
-    props.getFlats([
-        flatImages,
-        flatTitles,
-        flatProperties,
-        flatSummary,
-        flatPrice
-    ]);
+  props.getFlats([
+    flatImages,
+    flatTitles,
+    flatProperties,
+    flatSummary,
+    flatPrice
+  ]);
 }
+
 const dataHelper = {
-     useFetch: (url, props) => {
-        const [data, setData] = useState(null);
-        const [loading, setLoading] = useState(true);
-        const [error, setError] = useState(false);
-        const fetchData = () => {
-            fetchJsonp(url)
-                .then(response => response.json())
-                .then(data => {
-                    if (data.response["application_response_text"] === "unknown location") {
-                        setError(true);
-                        setLoading(false);
-                    } else {
-                        setError(false);
-                        setData(data);
-                        dataProcessing(data, props);
-                        setLoading(false);
-                    }
-                });
-        };
-        useEffect(() => {
-            fetchData();
-        }, [url]);
+  useFetch: (url, props) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const fetchData = () => {
+      fetchJsonp(url)
+        .then(response => response.json())
+        .then(data => {
+          if (
+            data.response["application_response_text"] === "unknown location"
+          ) {
+            setError(true);
+            setLoading(false);
+          } else {
+            setError(false);
+            setData(data);
+            dataProcessing(data, props);
+            setLoading(false);
+          }
+        });
+    };
+    useEffect(() => {
+      fetchData();
+    }, [url]);
 
-        return { data, loading, error };
-    },
+    return { data, loading, error };
+  },
+  constructQueryParams: ({ searchInput }) => {
+    const url = "https://api.nestoria.co.uk/api?";
+    let params = new URLSearchParams();
 
+    params.append("encoding", "json");
+    params.append("pretty", "1");
+    params.append("action", "search_listings");
+    params.append("country", "uk");
+    params.append("listing_type", "rent");
+    params.append("page", `1`);
+    params.append("place_name", `${searchInput}`);
+    return url + params;
+  }
 };
 
-
-
-export {dataHelper}
+export { dataHelper };
